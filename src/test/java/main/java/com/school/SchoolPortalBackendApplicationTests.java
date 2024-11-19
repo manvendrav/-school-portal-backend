6a760afced8a9b2507a3783c7b04b04b53f244d7
Manvendra Verma <manvendra.verma@netcracker.com>
2024-11-19 13:55:07 +0530

    first commit

---
+++ src/test/java/main/java/com/school/SchoolPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package main.java.com.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
